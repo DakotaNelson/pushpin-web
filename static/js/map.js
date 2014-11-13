42603991a88c8b2ae22eded7ae13e6cda852f52b
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-11-12 23:46:40 -0500

    markers now toggle

--- static/js/map.js
+++ static/js/map.js
@@ -7,8 +7,11 @@ var MAP = {
   radius: null,
   data: null,
   icon: {
-           'Twitter':"http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
-           'Flickr':"http://maps.google.com/mapfiles/ms/icons/orange-dot.png"
+          'Flickr': 'http://maps.google.com/mapfiles/ms/icons/orange-dot.png',
+          'Picasa': 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png',
+          'Shodan': 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
+          'Twitter': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+          'Youtube': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
         },
   elementId: 'map',
 
@@ -103,8 +106,8 @@ var MAP = {
   },
 
   setMarkers: function(place, source) {
-    for (var i = 0; i < window[source].length; i++) {
-        MAP.marker[source][i].setMap(place);
+    for (var i = 0; i < MAP.markers[source].length; i++) {
+        MAP.markers[source][i].setMap(place);
     }
   },
 
@@ -127,3 +130,13 @@ var MAP = {
     }
   },
 };
+
+  /*
+     example marker input here:
+     add_marker(
+     {position: new google.maps.LatLng(42.297276,-71.26462),title:"preschang",icon:"http://maps.google.com/mapfiles/ms/icons/orange-dot.png",map:map},
+     {details:"<table><tr><td class='prof_cell'><a href='https://farm4.staticflickr.com/3929/15391622816_6e1b496075.jpg' target='_blank'><img class='prof_img rounded' src='https://farm4.staticflickr.com/3929/15391622816_6e1b496075_t.jpg' /></a></td><td class='data_cell'>[<a href='http://flickr.com/photos/99579729@N06' target='_blank'>preschang</a>] Add #Babson College this week as a member of the #Goldman Sachs 10k small business program. Learning how to be a better #business man!<br /><span class='time'>2014-10-01 12:05:41</span></td></tr></table>"},
+     "flickr"
+     );
+  */
+
