2f062509fe6df454b27ce85f8c316017c8863740
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-12-06 00:15:34 -0500

    added media view

--- pushpin-app/map/urls.py
+++ pushpin-app/map/urls.py
@@ -4,7 +4,7 @@ from map import views
 
 urlpatterns = patterns('',
         url(r'location/(?P<locName>.+)/delete/$', views.deleteLocation, name='delete location'),
-        url(r'location/(?P<locName>.+?)/media/?$', views.mediaView, name='map location'),
+        url(r'location/(?P<locName>.+?)/media/?$', views.mediaView, name='media view'),
         url(r'location/(?P<locName>.+?)/?$', views.mapView, name='map location'),
         url(r'add-new-location/$', views.addLocation, name='track new location'),
         url(r'location/', views.noLocation, name='no location selected'),
