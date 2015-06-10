e370b250ad7a26f42d7d1416c0aaaa534ded690c
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2015-06-10 13:05:32 -0400

    fix django admin static files

--- config/web-run.sh
+++ config/web-run.sh
@@ -11,10 +11,8 @@ cd /root/pushpin-app && \
 cd /root/pushpin-app && \
   python3 deploy.py
 
-#ln -s /usr/local/lib/python3.4/dist-packages/django/contrib/admin/static/admin /root/static/
-# link the static assets for the djano admin pages
-
 exec /usr/local/bin/uwsgi --ini /root/pushpin-app/config/wsgi_config.ini
 
 #TODO don't run as root
+# shared volumes cause problems...
 #su -c 'exec /usr/local/bin/uwsgi --ini /root/pushpin-app/config/wsgi_config.ini' app
