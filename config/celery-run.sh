0fb6e94372f81825119385722700db7e5c7f52b6
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2015-06-11 13:51:08 -0400

    enable celerybeat

--- config/celery-run.sh
+++ config/celery-run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#cd /root/pushpin-app && /sbin/setuser app python3 manage.py celery beat
+python3 /root/pushpin-app/manage.py celery beat &
 
 exec python3 /root/pushpin-app/manage.py celery worker --concurrency=2
 
